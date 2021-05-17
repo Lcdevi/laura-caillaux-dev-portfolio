858443bd3ea5f791708e90319c1f6c6eab6d7954
Lcdevi <caillauxlaura@gmail.com>
2021-05-18 00:09:34 +0200

    slider

--- pages/work.vue
+++ pages/work.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="work">
 		<h1>work</h1>
-		<div v-for="project in projects" :key="project.id">
-			<Project
-				:project="project"
-			 />
+		<div id="carrousel">
+			<div class="item" v-for="project in projects" :key="project.id">
+				<Project
+					:project="project"
+				/>
+			</div>
 		</div>
 	</div>
 </template>
@@ -28,7 +30,10 @@ export default {
 	flex-direction: column;
 	align-items: center;
 	margin: 70px 0;
-
+	& #carrousel {
+		display: flex;
+		overflow: hidden;
+	}
 }
 
 </style>
