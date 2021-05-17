a0f1b8a88f407e6ebc42171e88ac164f8e44ef9c
Lcdevi <caillauxlaura@gmail.com>
2021-05-17 23:47:43 +0200

    add work component

---
+++ components/Project.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="project">
+    <div id="image"></div>
+    <div id="infos">
+      <h2>{{ project.title }}</h2>
+      -
+      <p class="inf">{{ project.infos }}</p>
+      -
+      <p class="inf">{{ project.stack }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+
+  mounted() {
+    console.log(this.project)    
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+#project {
+  margin: 40px 0;
+  & #image {
+    width: 600px;
+    height: 300px;
+    background-color: rgb(199, 199, 199);
+  }
+  & #infos {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    & p {
+      margin: 10px 0;
+    }
+    & h2 {
+      margin: 20px 0 10px 0;
+    } 
+  }
+}
+</style>
--- pages/work.vue
+++ pages/work.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="work">
 		<h1>work</h1>
+		<div v-for="project in projects" :key="project.id">
+			<Project
+				:project="project"
+			 />
+		</div>
 	</div>
 </template>
+
+<script>
+import projectData from "~/data/projects.json";
+
+export default {
+	data() {
+    return {
+      projects: projectData,
+    }
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 
@@ -11,6 +28,7 @@
 	flex-direction: column;
 	align-items: center;
 	margin: 70px 0;
+
 }
 
 </style>
